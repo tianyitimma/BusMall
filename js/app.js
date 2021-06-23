2ae4808501838e4f197b48c8247815897c5e09d4
mtygit <tim.ma0118@gmail.com>
2021-06-22 20:06:58 -0700

    added chart feature

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // Global varables
-const roundElem = document.getElementById('round');
 const mainElem = document.getElementById('main');
-const buttonElem = document.getElementById('button');
+const roundElem = document.getElementById('round');
+const divElem = document.getElementById('div');
 const leftImgElem = document.getElementById('leftImg');
 const leftNameElem = document.getElementById('leftName');
 const midImgElem = document.getElementById('midImg');
@@ -17,12 +17,6 @@ let rightProduct = null;
 let round = 5;
 let roundCounter = 0;
 
-// number of products to show each time
-let numOfProducts = 3;
-
-// const leftElem = document.getElementById('left');
-// const midElem = document.getElementById('mid');
-// const rightElem = document.getElementById('right');
 Product.allProducts = [];
 
 // object constructor function
@@ -35,16 +29,6 @@ function Product(name, imgPath){
   Product.allProducts.push(this);
 }
 
-// render a single product to html
-// Product.prototype.renderProduct = function(position) {
-//   const productImg = document.createElement('img');
-//   productImg.src = this.imgPath;
-//   position.appendChild(productImg);
-//   const productName = document.createElement('h3');
-//   productName.textContent = this.name;
-//   position.appendChild(productName);
-// }
-
 // render single product
 
 Product.prototype.renderProduct = function(namePosition, imgPosition) {
@@ -53,17 +37,33 @@ Product.prototype.renderProduct = function(namePosition, imgPosition) {
   this.shows++;
 }
 
+// random index generator
 
-// creating a function randomly generates n products
+function randomIndex () {
+  return Math.floor(Math.random() * Product.allProducts.length);
+}
+// creating a function randomly generates products and making sure not repeat twice in a row
 
 function productToShow(){
-  let randomArray = Product.allProducts.sort(() => 0.5 - Math.random());
-  let randomProducts = randomArray.slice(0,numOfProducts);
-  // in this case we know n is 3
-  console.log(randomProducts);
-  leftProduct = randomProducts[0];
-  midProduct = randomProducts[1];
-  rightProduct = randomProducts[2];
+  let currentProducts = [leftProduct, midProduct, rightProduct];
+
+  while (currentProducts.includes(leftProduct)) {
+    let index = randomIndex();
+    leftProduct = Product.allProducts[index];
+  }
+  currentProducts.push(leftProduct);
+
+  while (currentProducts.includes(midProduct)) {
+    let index = randomIndex();
+    midProduct = Product.allProducts[index];
+  }
+  currentProducts.push(midProduct);
+
+  while (currentProducts.includes(rightProduct)) {
+    let index = randomIndex();
+    rightProduct = Product.allProducts[index];
+  }
+  currentProducts.push(rightProduct);
 
   leftProduct.renderProduct(leftNameElem, leftImgElem);
   midProduct.renderProduct(midNameElem, midImgElem);
@@ -72,31 +72,6 @@ function productToShow(){
   roundElem.innerHTML = '';
   roundElem.textContent = `Round Number ${roundCounter}`;
 }
-
-
-
-// function productToShow(){
-//   let randomArray = Product.allProducts.sort(() => 0.5 - Math.random());
-//   let randomProducts = randomArray.slice(0,numOfProducts);
-//   // in this case we know n is 3
-
-//   for(let i = 0; i < numOfProducts; i++){
-//     // const divElem = document.createElement('div');
-//     // divElem.setAttribute('id', `${randomProducts[i].name}`);
-//     // mainElem.appendChild(divElem);
-//     // const imgElem = document.createElement('img');
-//     // imgElem.src = randomProducts[i].imgPath;
-//     // divElem.appendChild(imgElem);
-//     // const h3Elem = document.createElement('h3');
-//     // h3Elem.textContent = randomProducts[i].name;
-//     // divElem.appendChild(h3Elem);
-//     randomProducts[i].renderProduct();
-//     randomProducts[i].shows++;
-//   }
-//   roundCounter++;
-//   roundElem.innerHTML = '';
-//   roundElem.textContent = `Round Number ${roundCounter}`;
-// }
 
 // showing result button
 
@@ -111,65 +86,191 @@ function resultButton() {
 function handleClick(event){
   let id = event.target.id;
 
-  if (id.includes('left','right','mid')){
-    if(id.includes('left')){
-      leftProduct.clicked++;
-    } else if(id.includes('mid')){
-      midProduct.clicked++;
-    } else {
-      rightProduct.clicked++;
-    }
-    productToShow();
+  if(id === 'left' || id === 'leftImg' || id === 'leftName'){
+    leftProduct.clicked++;
+    alert('left product is selected');
+  } else if(id === 'mid' || id === 'midImg' || id === 'midName'){
+    midProduct.clicked++;
+    alert('middle product is selected');
+  } else if (id === 'right' || id === 'rightImg' || id === 'rightName'){
+    rightProduct.clicked++;
+    alert('right product is selected');
   }
-  if (roundCounter > round) {
+  if (roundCounter < round){
+    productToShow();
+  } else {
+    resultButton();
     mainElem.removeEventListener('click', handleClick);
-    showResults();
+    mainElem.addEventListener('click', showResults);
   }
 }
 
-function showResults() {
-  for(let product of Product.allProducts){
-    if (product.clicked > 0){
-      const pElem = document.createElement('p');
-      pElem.textContent = `${product.name} had ${product.clicked} votes, and was seen ${product.shows} times.`;
-      mainElem.appendChild(pElem);
+function showResults(event) {
+  let text = event.target.textContent;
+  if(text === 'View Results'){
+    for(let product of Product.allProducts){
+      if (product.clicked > 0){
+        const pElem = document.createElement('p');
+        pElem.textContent = `${product.name} had ${product.clicked} votes, and was seen ${product.shows} times.`;
+        mainElem.appendChild(pElem);
+      }
     }
   }
-  mainElem.removeEventListener('submit', showResults);
+  mainElem.removeEventListener('click', showResults);
+  // const canvasElem = document.createElement('canvas');
+  // canvasElem.setAttribute
+  addResultChart();
+}
+
+// function to add a chart
+
+function addResultChart() {
+  const productNames = [];
+  const productClicked = [];
+  const productShown = [];
+
+  for (let product of Product.allProducts){
+    productNames.push(product.name);
+    productClicked.push(product.clicked);
+    productShown.push(product.shows);
+  }
+
+  const ctx = document.getElementById('productChart').getContext('2d');
+
+  const productChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: '# of Votes',
+        data: productClicked,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)'
+        ],
+        borderWidth: 1
+      }, {
+        label: '# of Shown',
+        data: productShown,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
 }
 
 // add event listener for clicking product
 
 mainElem.addEventListener('click', handleClick);
 
-
-
-
-
 // input all the products
-new Product('bag', './img/bag.jpg');
-new Product('banana', './img/banana.jpg');
-new Product('bathroom', './img/bathroom.jpg');
-new Product('boots', './img/boots.jpg');
-new Product('breakfast', './img/breakfast.jpg');
-new Product('bubblegum', './img/bubblegum.jpg');
-new Product('chair', './img/chair.jpg');
-new Product('cthulhu', './img/cthulhu.jpg');
-new Product('dog-duck', './img/dog-duck.jpg');
-new Product('dragon', './img/dragon.jpg');
-new Product('pen', './img/pen.jpg');
-new Product('pet-sweep', './img/pet-sweep.jpg');
-new Product('scissors', './img/scissors.jpg');
-new Product('shark', './img/shark.jpg');
-new Product('sweep', './img/sweep.png');
-new Product('tauntaun', './img/tauntaun.jpg');
-new Product('unicorn', './img/unicorn.jpg');
-new Product('water-can', './img/water-can.jpg');
-new Product('wine-glass', './img/wine-glass.jpg');
-
+function createProducts() {
+  new Product('bag', './img/bag.jpg');
+  new Product('banana', './img/banana.jpg');
+  new Product('bathroom', './img/bathroom.jpg');
+  new Product('boots', './img/boots.jpg');
+  new Product('breakfast', './img/breakfast.jpg');
+  new Product('bubblegum', './img/bubblegum.jpg');
+  new Product('chair', './img/chair.jpg');
+  new Product('cthulhu', './img/cthulhu.jpg');
+  new Product('dog-duck', './img/dog-duck.jpg');
+  new Product('dragon', './img/dragon.jpg');
+  new Product('pen', './img/pen.jpg');
+  new Product('pet-sweep', './img/pet-sweep.jpg');
+  new Product('scissors', './img/scissors.jpg');
+  new Product('shark', './img/shark.jpg');
+  new Product('sweep', './img/sweep.png');
+  new Product('tauntaun', './img/tauntaun.jpg');
+  new Product('unicorn', './img/unicorn.jpg');
+  new Product('water-can', './img/water-can.jpg');
+  new Product('wine-glass', './img/wine-glass.jpg');
+}
+createProducts();
 productToShow();
-
-resultButton();
-
-// add event listener for submit button
-mainElem.addEventListener('submit', showResults);
